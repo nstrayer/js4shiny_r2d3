e3a2aee2c5b96a1a0c829ab9b9d518fc7e1fd72c
Nick Strayer <nick.strayer@gmail.com>
2020-01-27 11:09:39 -0800

    Basic shiny app to predict two tree types using dropdowns.

---
+++ regress_trees/app.R
@@ -0,0 +1,88 @@
+library(shiny)
+library(tidyverse)
+library(glmnet)
+library(r2d3)
+
+# Read in data 
+trees <- read_csv("../trees_cleaned.csv") %>% 
+    select(-id, -street, -scientific_name)
+
+# Get summary level info for plotting
+tree_species <- trees %>% 
+    group_by(common_name) %>% 
+    summarise_if(is.numeric, mean) %>% 
+    group_by(common_name) %>% 
+    summarise(num_obs = n(),
+              dollar_benefits = mean(overall_benefits_dollar_value),
+              co2_benefits = mean(co2_benefits_totalco2_lbs))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    # Application title
+    titlePanel("Which Tree Is That?"),
+    selectInput('species_1', 
+                label =  "Species 1", 
+                choices = tree_species$common_name, 
+                selected = sample(tree_species$common_name, 1)),
+    selectInput('species_2', 
+                label =  "Species 2", 
+                choices = tree_species$common_name,
+                selected = sample(tree_species$common_name, 1)),
+    plotOutput("regressionOutput")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    selected_species <- reactive({
+        c(input$species_1, input$species_2)
+    })
+    
+    output$regressionOutput <- renderPlot({
+        species_1 <- selected_species()[1]
+        species_2 <- selected_species()[2]
+        
+        if(species_1 == species_2){
+            stop("Need two different selected species")
+        }
+        
+        species_data <- trees %>% filter(common_name == species_1 | common_name == species_2)  
+        
+        predictors_df <- species_data %>% 
+            select(-common_name) %>% 
+            rename_if(is.character, function(name) paste0(name,"__")) %>% 
+            rename_if(is.numeric, function(name) paste0("numeric__",name)) 
+        
+        predictors <-  model.matrix( ~ ., predictors_df)
+        is_species_1 <- species_data$common_name == species_1
+        
+        fit <- cv.glmnet(x = predictors, y = is_species_1, family = "binomial", nfolds = 5)
+        
+        plot_title <- glue::glue("Coeffients of model to predict \"{species_1}\" or \"{species_2}\"")
+        plot_subtitle <- glue::glue("LASSO regression. Predicting 1 = \"{species_1}\", 0 = \"{species_2}\"")
+        
+        coef(fit) %>% {
+            tibble(
+                variable = rownames(.),
+                coefficient = .[,1]
+            )} %>% 
+            filter(coefficient != 0, variable != "(Intercept)") %>% 
+            arrange(coefficient) %>% 
+            separate(variable, into = c("category", "variable"), sep = "__") %>% 
+            mutate(
+                variable = str_replace_all(variable, "_", " "),
+                positive = coefficient > 0) %>% 
+            ggplot( aes(x = coefficient, y = reorder(variable, coefficient), color = positive )) +
+                geom_point() +
+                geom_segment(aes(xend = 0, yend = variable)) +
+                facet_grid(rows = vars(category), space = "free_y", scales = "free_y", switch = "both") +
+                guides(color = FALSE) +
+                geom_vline(xintercept = 0, alpha = 0.4) +
+                labs(title = plot_title,
+                     subtitle = plot_subtitle, 
+                     y = "")
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
