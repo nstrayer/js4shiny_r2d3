c3bf09e8dd6ab7d3f1d44ca814d0b2b41d513fa5
Nick Strayer <nick.strayer@gmail.com>
2020-01-27 11:46:51 -0800

    Added message to shiny sending code.

--- benefits_visualization.js
+++ benefits_visualization.js
@@ -170,6 +170,15 @@ simulation.on("tick", () => {
 
 function submit_selection(){
   console.log(`Selected ${current_selection[0]} and ${current_selection[1]}`);
+  
+  if(Shiny){
+    console.log("Sending to shiny");
+    Shiny.setInputValue(
+      "selected_species", 
+      current_selection,
+      {priority: "event"}
+    );
+  }
 }
 
 // Helper functions
